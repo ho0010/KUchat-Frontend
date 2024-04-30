10579e910ecf004476e72071c910bb6d8eb22de7
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2024-04-30 15:40:09 +0900

    Feat: Login => signup navigate

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Container } from "./Home.styles";
 
 function Home() {
   return (
     <div>
-      <Container>안녕하세요 !</Container>
+      <div>안녕하세요 !</div>
       <Link to="/signup">회원가입</Link>
       <Link to="/login">로그인</Link>
     </div>
--- src/routes/Login/Login.tsx
+++ src/routes/Login/Login.tsx
@@ -1,22 +1,29 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Splash from "./Splash";
-import stlyes from "./Login.module.css";
+import styles from "./Login.module.css";
 import KUchatImg from "../../assets/img/KUchatImg.svg";
 
 function Login() {
   const [showSplash, setShowSplash] = useState(true);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const timer = setTimeout(() => setShowSplash(false), 3000);
     return () => clearTimeout(timer);
   }, []);
+
+  const handleClick = () => {
+    navigate("/signup");
+  };
   return showSplash ? (
     <Splash />
   ) : (
-    <div className={stlyes.root}>
-      <div className={stlyes.ctn}>
+    <div className={styles.root}>
+      <div className={styles.ctn}>
         <img src={KUchatImg} alt="KU chat logo" />
-        <div className={stlyes.splashText}>KU chat</div>
+        <div className={styles.splashText}>KU chat</div>
+        <button onClick={handleClick}>d</button>
       </div>
     </div>
   );

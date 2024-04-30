b8246a979a6ad76466cb417b3d30a93303bbbf7d
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2024-04-30 16:02:16 +0900

    Refactor: routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,19 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./Home/Home";
 import Signup from "./Signup/Signup";
 import Login from "./Login/Login";
+import App from "./App";
 
 function Router() {
   const router = createBrowserRouter([
-    { path: "/", element: <Home /> },
-    { path: "/signup", element: <Signup /> },
-    { path: "/login", element: <Login /> },
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        { path: "", element: <Home /> },
+        { path: "/signup", element: <Signup /> },
+        { path: "/login", element: <Login /> },
+      ],
+    },
   ]);
 
   return <RouterProvider router={router} />;

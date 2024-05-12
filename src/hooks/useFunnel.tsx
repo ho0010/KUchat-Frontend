a977ca9b49ec715aa4bd3a044691c9e1ff4aed0c
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2024-05-12 15:50:40 +0900

    Feat: react-hook-form 적용중

--- src/hooks/useFunnel.tsx
+++ src/hooks/useFunnel.tsx
@@ -5,7 +5,6 @@ export interface StepProps {
   children: ReactNode;
 }
 
-
 export interface FunnelProps {
   children: Array<ReactElement<StepProps>>;
 }
@@ -13,19 +12,19 @@ export interface FunnelProps {
 export const useFunnel = (defaultStep: string) => {
   const [step, setStep] = useState(defaultStep);
 
-  //각 단계를 나타내는 step 컴포너트
-  //children을 통해 각 스텝의 컨텐츠 렌더링
+  // 각 단계를 나타내는 step 컴포너트
+  // children을 통해 각 스텝의 컨텐츠 렌더링
   const Step = (props: StepProps): ReactElement => {
     return <>{props.children}</>;
-  }
-  
-  //여러 단계의 Step 컴포넌트 중 현재 활성화된 스텝을 렌더링하는 funnel
+  };
+
+  // 여러 단계의 Step 컴포넌트 중 현재 활성화된 스텝을 렌더링하는 funnel
   // find를 통해 Step 중 현재 Step을 찾아 렌더링
-  const Funnel = ({children} : FunnelProps) => {
+  const Funnel = ({ children }: FunnelProps) => {
     const targetStep = children.find((child) => child.props.name === step);
-  
+
     return <>{targetStep}</>;
   };
 
-  return {Funnel,Step,setStep,currentStep:step} as const;
+  return { Funnel, Step, setStep, currentStep: step } as const;
 };
--- src/routes/Signup/components/Language.tsx
+++ src/routes/Signup/components/Language.tsx
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { useFormContext } from "react-hook-form";
 
-export default function Language() {
-  return <div>Language</div>;
-}
+const Language = () => {
+  const { register, errors, status } = useFormContext();
+
+  return (
+    <input
+      type="text"
+      status={status}
+      {...register("school")}
+      errorMessage={
+        errors.school && typeof errors.school.message === "string"
+          ? errors.school.message
+          : undefined
+      }
+      placeholder="학교 이름을 검색해주세요."
+      onChange={inputchangeHandler}
+    />
+  );
+};
+
+export default Language;

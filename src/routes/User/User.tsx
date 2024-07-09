c81c88d7108ae7f2c7dd987e79f66ad1cf836a8b
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2024-07-09 17:59:24 +0900

    Feat: User Header Btn

--- src/routes/User/User.tsx
+++ src/routes/User/User.tsx
@@ -3,12 +3,14 @@ import styles from "./User.module.css";
 import ChatList from "./components/ChatList";
 import FindFriend from "./components/FindFriend";
 import UserProfile from "./components/Profile";
-import ChatImg from "../../assets/img/User_chat.svg";
+import ChatImg from "../../assets/img/User_Chat.svg";
 import FriendImg from "../../assets/img/User_Friend.svg";
 import ProfileImg from "../../assets/img/User_Profile.svg";
 import selectedChatImg from "../../assets/img/User_Selected_chat.svg";
 import selectedFriendImg from "../../assets/img/User_Selected_Friend.svg";
 import selectedProfileImg from "../../assets/img/User_Selected_Profile.svg";
+import ChatPlusImg from "../../assets/img/User_Chat_Plus.svg";
+import ProfileSettingImg from "../../assets/img/User_Profile_Setting.svg";
 
 type UserStep = "chatList" | "friend" | "profile";
 
@@ -18,9 +20,23 @@ const User = () => {
   return (
     <div className={styles.root}>
       <div className={styles.header}>
-        {step === "chatList" && <h1>채팅 목록</h1>}
+        {step === "chatList" && (
+          <div className={styles.detailHeader}>
+            <h1>채팅 목록</h1>
+            <button>
+              <img src={ChatPlusImg} alt="채팅 추가" />
+            </button>
+          </div>
+        )}
         {step === "friend" && <h1>친구 찾기</h1>}
-        {step === "profile" && <h1>나의 프로필</h1>}
+        {step === "profile" && (
+          <div className={styles.detailHeader}>
+            <h1>나의 프로필</h1>
+            <button>
+              <img src={ProfileSettingImg} alt="설정" />
+            </button>
+          </div>
+        )}
       </div>
       <div className={styles.body}>
         {step === "chatList" && <ChatList />}
